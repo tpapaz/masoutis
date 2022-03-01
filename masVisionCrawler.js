8908e999bea6974f19fd79fc39f8ff0d65f7bc07
Tasos Papazoglou Chalikias <tasos@tpapazoglou.com>
2022-03-01 15:10:01 +0200

    Added final_price field (after discount). Changed a bit the mysql fields in order to be properly documented.

--- masVisionCrawler.js
+++ masVisionCrawler.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 app.post('/update', async function(request, response){
 
     console.log("Started update");
-    //await updateFiles();
+    await updateFiles();
     await updateDB();
     response.send("Update successful!");
     console.log("Update successful");
